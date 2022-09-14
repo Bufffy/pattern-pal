855533063a6c479e956e004dde7db6837caff384
Antoinette Lovelace <antoinette.lovelace@thoughtworks.com>
2022-09-13 19:51:19 -0700

    replace mocks with patch to limit scope

--- unit_tests/test_pattern_updater.py
+++ unit_tests/test_pattern_updater.py
@@ -1,82 +1,65 @@
 import pytest
-from unittest.mock import Mock
-# dependencies
-from logic.size_info_updater import SizeInfoUpdater
+from unittest.mock import patch
 from logic.pattern_updater import PatternUpdater
-from logic.pattern_parser import PatternParser
-from facades.pdf_parser import PdfParser
 
 
-@pytest.mark.parametrize("pdf_file", ("bennietop.pdf", "BENNIETOP.PDF"))
-def test__update_pattern__accepts_pdf_file_format(pdf_file: str):
-    size = 2
-    PdfParser.get_all_text = Mock()
-    PdfParser.get_all_text.return_value = "2 (3) 4 (5) 6 balls of yarn"
-    PatternParser.get_sizing_info = Mock()
-    PatternParser.get_sizing_info.return_value = ["2 (3) 4 (5) 6"]
+@patch("logic.size_info_updater.SizeInfoUpdater.replace_size_info_with_selected_size_info")
+@patch("logic.pattern_parser.PatternParser.get_sizing_info")
+@patch("facades.pdf_parser.PdfParser.get_all_text")
+class TestPatternUpdater:
+    @pytest.mark.parametrize("pdf_file", ("bennietop.pdf", "BENNIETOP.PDF"))
+    def test__update_pattern__accepts_pdf_file_format(self, mock_get_all_text, mock_get_sizing_info, mock_replace_size_info, pdf_file):
+        size = 2
+        mock_get_all_text.return_value = "2 (3) 4 (5) 6 balls of yarn"
+        mock_get_sizing_info.return_value = ["2 (3) 4 (5) 6"]
 
-    PatternUpdater.update_pattern(pdf_file, size)
+        PatternUpdater.update_pattern(pdf_file, size)
 
+    def test__update_pattern__raises_exception_when_file_format_is_not_pdf(self, mock_get_all_text, mock_get_sizing_info, mock_replace_size_info):
+        size = 4
+        non_pdf_file = "BennieTop.txt"
 
-def test__update_pattern__raises_exception_when_file_format_is_not_pdf():
-    size = 4
-    non_pdf_file = "BennieTop.txt"
+        with pytest.raises(ValueError) as e_info:
+            PatternUpdater.update_pattern(non_pdf_file, size)
 
-    with pytest.raises(ValueError) as e_info:
-        PatternUpdater.update_pattern(non_pdf_file, size)
+        assert str(e_info.value) == "File type is not pdf."
 
-    assert str(e_info.value) == "File type is not pdf."
+    def test__update_pattern__gets_text_from_file(self, mock_get_all_text, mock_get_sizing_info, mock_replace_size_info):
+        pdf_file = "BennieTop.pdf"
+        size = 2
+        mock_get_all_text.return_value = "2 (3) 4 (5) 6 balls of yarn"
 
+        PatternUpdater.update_pattern(pdf_file, size)
 
-def test__update_pattern__gets_text_from_file():
-    pdf_file = "BennieTop.pdf"
-    size = 2
-    PdfParser.get_all_text = Mock()
-    PdfParser.get_all_text.return_value = "2 (3) 4 (5) 6 balls of yarn"
+        mock_get_all_text.assert_called_with(pdf_file)
 
-    PatternUpdater.update_pattern(pdf_file, size)
+    def test__update_pattern__gets_sets_of_size_info_from_pdf_text(self, mock_get_all_text, mock_get_sizing_info, mock_replace_size_info):
+        size = 2
+        pdf_file_text = "2 (3) 4 (5) 6 balls of yarn"
+        mock_get_all_text.return_value = pdf_file_text
 
-    PdfParser.get_all_text.assert_called_with(pdf_file)
+        PatternUpdater.update_pattern("BennieTop.pdf", size)
 
+        mock_get_sizing_info.assert_called_with(pdf_file_text)
 
-def test__update_pattern__gets_sets_of_size_info_from_pdf_text():
-    size = 2
-    pdf_file_text = "2 (3) 4 (5) 6 balls of yarn"
-    PdfParser.get_all_text = Mock()
-    PdfParser.get_all_text.return_value = pdf_file_text
-    PatternParser.get_sizing_info = Mock()
-    SizeInfoUpdater.replace_size_info_with_selected_size_info = Mock()
+    def test__update_pattern__replaces_sets_of_size_info_with_selected_size_info(self, mock_get_all_text, mock_get_sizing_info, mock_replace_size_info):
+        size = 2
+        text = "2 (3) 4 (5) 6 balls of yarn"
+        sizing_info = ["2 (3) 4 (5) 6"]
+        mock_get_all_text.return_value = text
+        mock_get_sizing_info.return_value = sizing_info
 
-    PatternUpdater.update_pattern("BennieTop.pdf", size)
+        PatternUpdater.update_pattern("BennieTop.pdf", size)
 
-    PatternParser.get_sizing_info.assert_called_with(pdf_file_text)
+        mock_replace_size_info.assert_called_with(text, sizing_info, size)
 
+    def test__update_pattern__returns_updated_text_with_selected_size_info(self, mock_get_all_text, mock_get_sizing_info, mock_replace_size_info):
+        updated_pattern = "6 balls of yarn"
+        size = 4
+        mock_get_all_text.return_value = "2 (3) 4 (5) 6 balls of yarn"
+        mock_get_sizing_info.return_value = ["2 (3) 4 (5) 6"]
+        mock_replace_size_info.return_value = updated_pattern
 
-def test__update_pattern__replaces_sets_of_size_info_with_selected_size_info():
-    size = 2
-    text = "2 (3) 4 (5) 6 balls of yarn"
-    sizing_info = ["2 (3) 4 (5) 6"]
-    PdfParser.get_all_text = Mock()
-    PdfParser.get_all_text.return_value = text
-    PatternParser.get_sizing_info = Mock()
-    PatternParser.get_sizing_info.return_value = sizing_info
-    SizeInfoUpdater.replace_size_info_with_selected_size_info = Mock()
+        output = PatternUpdater.update_pattern("BennieTop.pdf", size)
 
-    PatternUpdater.update_pattern("BennieTop.pdf", size)
-
-    SizeInfoUpdater.replace_size_info_with_selected_size_info.assert_called_with(text, sizing_info, size)
-
-
-def test__update_pattern__returns_updated_text_with_selected_size_info():
-    updated_pattern = "6 balls of yarn"
-    size = 4
-    PdfParser.get_all_text = Mock()
-    PdfParser.get_all_text.return_value = "2 (3) 4 (5) 6 balls of yarn"
-    PatternParser.get_sizing_info = Mock()
-    PatternParser.get_sizing_info.return_value = ["2 (3) 4 (5) 6"]
-    SizeInfoUpdater.replace_size_info_with_selected_size_info = Mock()
-    SizeInfoUpdater.replace_size_info_with_selected_size_info.return_value = updated_pattern
-
-    output = PatternUpdater.update_pattern("BennieTop.pdf", size)
-
-    assert str(output) == updated_pattern
+        assert str(output) == updated_pattern
--- unit_tests/test_size_info_updater.py
+++ unit_tests/test_size_info_updater.py
@@ -1,58 +1,58 @@
-import pytest
-from unittest.mock import Mock
+from unittest.mock import patch
 from logic.size_info_updater import SizeInfoUpdater
 
 
-def test__replace_size_info_with_selected_size_info__updates_text_with_one_set_of_sizing_info():
-    text = "2 (3) 4 (5) 6 balls of yarn"
-    set_of_size_info = ["2 (3) 4 (5) 6"]
-    size = 2
-    expected = "4 balls of yarn"
+@patch("logic.size_info_updater.SizeInfoUpdater.trim_size_info")
+class TestSizeInfoUpdater:
+    def test__replace_size_info_with_selected_size_info__updates_text_with_one_set_of_sizing_info(
+            self, mock_trim_size_info):
+        mock_trim_size_info.return_value = "4"
+        expected = "4 balls of yarn"
 
-    output = SizeInfoUpdater.replace_size_info_with_selected_size_info(text, set_of_size_info, size)
+        output = SizeInfoUpdater.replace_size_info_with_selected_size_info("2 (3) 4 (5) 6 balls of yarn",
+                                                                           ["2 (3) 4 (5) 6"],
+                                                                           2)
 
-    assert str(output) == expected
+        assert str(output) == expected
 
+    def test__replace_size_info_with_selected_size_info__updates_text_with_multiple_sets_of_sizing_info(
+            self, mock_trim_size_info):
+        size_info = "2 (3) 4 (5) 6"
+        more_size_info = "110 (115) 120 (125) 130"
+        mock_trim_size_info.side_effect = ["6", "130"]
+        expected = "6 balls of yarn\nk 130 stitches"
 
-@pytest.mark.parametrize(("size", "expected"), [(4, "6 balls of yarn\nk 130 stitches"), (0, "2 balls of yarn\nk 110 "
-                                                                                            "stitches")])
-def test__replace_size_info_with_selected_size_info__updates_text_with_multiple_sets_of_sizing_info(size, expected):
-    size_info = "2 (3) 4 (5) 6"
-    more_size_info = "110 (115) 120 (125) 130"
-    text = f"{size_info} balls of yarn\nk {more_size_info} stitches"
+        output = SizeInfoUpdater.replace_size_info_with_selected_size_info(f"{size_info} balls of yarn\nk "
+                                                                           f"{more_size_info} stitches",
+                                                                           [size_info, more_size_info],
+                                                                           4)
 
-    output = SizeInfoUpdater.replace_size_info_with_selected_size_info(text,
-                                                                       [size_info, more_size_info],
-                                                                       size)
+        assert str(output) == expected
 
-    assert str(output) == expected
+    def test__trim_size_info__removes_symbols_from_size_info(self, mock_trim_size_info):
+        symbols = ["(", ")"]
+        size_info = "(56)"
+        mock_trim_size_info.return_value = "56"
+        expected = "56"
 
+        output = SizeInfoUpdater.trim_size_info(symbols, size_info)
 
-def test__trim_size_info__removes_symbols_from_size_info():
-    symbols = ["(", ")"]
-    size_info = "(56)"
-    expected = "56"
+        assert str(output) == expected
 
-    output = SizeInfoUpdater.trim_size_info(symbols, size_info)
+    def test__replace_size_info_with_selected_size_info__removes_parentheses_from_size_info(self,
+                                                                                            mock_trim_size_info):
+        parentheses = ["(", ")"]
+        mock_trim_size_info.return_value = "5"
 
-    assert str(output) == expected
+        SizeInfoUpdater.replace_size_info_with_selected_size_info("2 (3) 4 (5) 6 balls of yarn", ["2 (3) 4 (5) 6"], 3)
 
+        mock_trim_size_info.assert_called_with(parentheses, "(5)")
 
-def test__replace_size_info_with_selected_size_info__removes_parentheses_from_size_info():
-    parentheses = ["(", ")"]
-    SizeInfoUpdater.trim_size_info = Mock()
-    SizeInfoUpdater.trim_size_info.return_value = "5"
+    def test__replace_size_info_with_selected_size_info__returns_trimmed_size_info(self, mock_trim_size_info):
+        mock_trim_size_info.return_value = "5"
+        expected = "5 balls of yarn"
 
-    SizeInfoUpdater.replace_size_info_with_selected_size_info("2 (3) 4 (5) 6 balls of yarn", ["2 (3) 4 (5) 6"], 3)
-
-    SizeInfoUpdater.trim_size_info.assert_called_with(parentheses, "(5)")
-
-
-def test__replace_size_info_with_selected_size_info__returns_trimmed_size_info():
-    SizeInfoUpdater.trim_size_info = Mock()
-    SizeInfoUpdater.trim_size_info.return_value = "5"
-    expected = "5 balls of yarn"
-
-    output = SizeInfoUpdater.replace_size_info_with_selected_size_info("2 (3) 4 (5) 6 balls of yarn", ["2 (3) 4 (5) 6"],
-                                                                       3)
-    assert str(output) == expected
+        output = SizeInfoUpdater.replace_size_info_with_selected_size_info("2 (3) 4 (5) 6 balls of yarn",
+                                                                           ["2 (3) 4 (5) 6"],
+                                                                           3)
+        assert str(output) == expected
